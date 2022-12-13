a655819ceb488640973cd29452fc3bd508e348ef
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-12-13 19:23:39 +0100

    imroved output file format

--- utils.py
+++ utils.py
@@ -14,5 +14,12 @@ def read_files(input_data: list[str]) -> list[str]:
 
 def write_to_txt(input_data: dict):
     '''writes input to a file'''
-    with open(f"run_times-{input_data['day']}.txt","x", encoding="utf8"):
+    with open(f"run_times-{input_data['day']}.txt","a", encoding="utf8") as file:
         print("file created")
+
+        file.write(f"Run time analisys for {input_data['day']}\n\n\n")
+
+
+        file.write("\tStart of check,\tDuration\n")
+        for t, d in zip(input_data['check_time'], input_data['check_dura']):
+            file.write(f"\t{t},\t{d}\n")
